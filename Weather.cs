92195d51e0bdfc388776817b05cbcc46b7757c75
Student 254014 <254014@student.pwr.edu.pl>
2022-03-28 13:34:05 +0200

    Weather class in progess

--- Weather.cs
+++ Weather.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    internal class Weather
+    class WeatherInfo
     {
+        public class coord
+        {
+            double lon { get; set; }
+            double lat { get; set; }
+        }
+
+        public class weather
+        {
+            string main { get; set; }
+            string description { get; set; }
+            string icon { get; set; }
+        }
+
+        public class main
+        {
+            double temp { get; set; }
+            double feels_like { get; set; }
+            double temp_min { get; set; }
+            double temp_max { get; set; }
+            double pressure { get; set; }
+            double humidity { get; set; }
+            double sea_level { get; set; }
+            double grnd_level { get; set; }
+        }
+
+
+
+
     }
 }
